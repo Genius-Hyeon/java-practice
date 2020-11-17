b091cee6f616b896a258bdc1a7ee302a641bc48d
지니어스현 <jhlee0912@icloud.com>
2020-11-17 23:52:00 +0900

    Update PersonDriver.java

--- Project1110/src/kr/or/iei/lambda/PersonDriver.java
+++ Project1110/src/kr/or/iei/lambda/PersonDriver.java
@@ -4,7 +4,7 @@ public class PersonDriver {
 	public static void main(String[] args) {
 		Person person = new Person();
 		person.hi((a, b) -> {
-			System.out.println("¾îÂ¼±¸ÀúÂ¼±¸");
+			System.out.println("ì–´ì©Œêµ¬ì €ì©Œêµ¬");
 			System.out.println("parameter number is " + a + ", " + b);
 			return 7;
 		});
